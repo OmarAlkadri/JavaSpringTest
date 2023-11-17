4387f70344f5c8ef9863ae1b8558e4dfdffcf6d2
OmarAlkadri <omar.alkadri@hitatek.com>
2023-11-17 13:49:22 +0300

    Added my project

---
+++ src/main/java/users/users.java
@@ -0,0 +1,30 @@
+package users;
+
+public class users {
+    private Long _id;
+    private String name;
+    private Long[] userID;
+
+    
+    public Long get_id() {
+        return _id;
+    }
+    public void set_id(Long _id) {
+        this._id = _id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Long[] getUserID() {
+        return userID;
+    }
+    public void setUserID(Long[] userID) {
+        this.userID = userID;
+    }
+
+
+    
+}
